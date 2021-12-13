2547da5413b7423a64b814aaea2133cc6c7d917d
Bijan Saniee <bsaniee@gmail.com>
2021-12-12 21:36:52 -0500

    config added, models.py added

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 from flask import Flask
 
+## Import SQL alchemy and os
+
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
 ## Create application Object
 
 app = Flask(__name__)
+## app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+## Routes
 
 @app.get("/")
 def home():
